8e2c585169c6a7ae4d27a38078a5455c4588b84c
Gerrit Van Leeuwen <grrtvnlw@gmail.com>
2021-08-27 11:38:30 -0400

    updated components to use hooks

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -8,13 +8,14 @@ import "./collection.styles.scss";
 
 const CollectionPage = ({ collection }) => {
   const { title, items } = collection;
+
   return (
     <div className="collection-page">
       <h2 className="title">{title}</h2>
-      <div className='items'>
-        {
-          items.map(item => <CollectionItem key={item.id} item={item} />)
-        }
+      <div className="items">
+        {items.map((item) => (
+          <CollectionItem key={item.id} item={item} />
+        ))}
       </div>
     </div>
   );
